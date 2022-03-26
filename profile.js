07a42711ce2d71bd0105a12c89f3ecfec623a518
Andrew Cring <amcring1@gmail.com>
2022-03-26 10:17:13 -0700

    Adding the javascript for the buttons about Drew's favorites

---
+++ profile.js
@@ -0,0 +1,3 @@
+document.getElementById("color").addEventListener("click", () => { alert("Drew's favorite color is green.") })
+document.getElementById("place").addEventListener("click", () => { alert("Drew's favorite place is San Diego, California.") })
+document.getElementById("ritual").addEventListener("click", () => { alert("Drew's favorite ritual is a cup of coffee ebery morning.") })
